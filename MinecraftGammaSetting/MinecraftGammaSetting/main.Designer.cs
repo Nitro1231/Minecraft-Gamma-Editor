4b02a67c0d0ee50ebe84a98bc92e97a1a0df40ce
Nitro <nitro0@naver.com>
2020-01-03 01:20:30 +0900

    The first stable version commit. (V: 1)

--- MinecraftGammaSetting/MinecraftGammaSetting/main.Designer.cs
+++ MinecraftGammaSetting/MinecraftGammaSetting/main.Designer.cs
@@ -28,16 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
             this.topPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.close = new System.Windows.Forms.Panel();
             this.min = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
             this.gammaBar = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.setButton = new System.Windows.Forms.Button();
             this.gammaTextBox = new System.Windows.Forms.TextBox();
+            this.info = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.topPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gammaBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // topPanel
@@ -45,7 +55,7 @@
             this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
             this.topPanel.Controls.Add(this.close);
             this.topPanel.Controls.Add(this.min);
-            this.topPanel.Controls.Add(this.label3);
+            this.topPanel.Controls.Add(this.title);
             this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.topPanel.Location = new System.Drawing.Point(0, 0);
             this.topPanel.Name = "topPanel";
@@ -73,16 +83,16 @@
             this.min.TabIndex = 5;
             this.min.Click += new System.EventHandler(this.Min_Click);
             // 
-            // label3
+            // title
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 6);
-            this.label3.Margin = new System.Windows.Forms.Padding(5, 6, 3, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(142, 12);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Minecraft Gamma Editor";
-            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(45, 6);
+            this.title.Margin = new System.Windows.Forms.Padding(5, 6, 3, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(142, 12);
+            this.title.TabIndex = 6;
+            this.title.Text = "Minecraft Gamma Editor";
+            this.title.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
             // 
             // gammaBar
             // 
@@ -103,17 +113,18 @@
             this.label1.Size = new System.Drawing.Size(154, 12);
             this.label1.TabIndex = 5;
             this.label1.Text = "Change Gamma value to: ";
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
             // 
-            // button1
+            // setButton
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(10, 108);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(330, 35);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Set";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Set_Click);
+            this.setButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.setButton.Location = new System.Drawing.Point(10, 109);
+            this.setButton.Name = "setButton";
+            this.setButton.Size = new System.Drawing.Size(306, 35);
+            this.setButton.TabIndex = 6;
+            this.setButton.Text = "Set";
+            this.setButton.UseVisualStyleBackColor = true;
+            this.setButton.Click += new System.EventHandler(this.setButton_Click);
             // 
             // gammaTextBox
             // 
@@ -129,25 +140,94 @@
             this.gammaTextBox.TextChanged += new System.EventHandler(this.gammaTextBox_TextChanged);
             this.gammaTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gammaTextBox_KeyPress);
             // 
+            // info
+            // 
+            this.info.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(229)))), ((int)(((byte)(56)))));
+            this.info.Location = new System.Drawing.Point(324, 127);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(16, 16);
+            this.info.TabIndex = 8;
+            this.info.Click += new System.EventHandler(this.Info_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(8, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(332, 98);
+            this.label2.TabIndex = 9;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "icons by Icons8";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MinecraftGammaSetting.Properties.Resources.icons8_blog_32;
+            this.pictureBox3.Location = new System.Drawing.Point(86, 262);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.PictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MinecraftGammaSetting.Properties.Resources.icons8_home_page_32;
+            this.pictureBox2.Location = new System.Drawing.Point(48, 262);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MinecraftGammaSetting.Properties.Resources.icons8_github_32;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 262);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.ClientSize = new System.Drawing.Size(350, 155);
+            this.ClientSize = new System.Drawing.Size(350, 300);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.info);
             this.Controls.Add(this.gammaTextBox);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.setButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gammaBar);
             this.Controls.Add(this.topPanel);
+            this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "main";
             this.Text = "MinecraftGammaSetting";
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.Resize += new System.EventHandler(this.Main_Resize);
             this.topPanel.ResumeLayout(false);
             this.topPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gammaBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,11 +238,17 @@
         private System.Windows.Forms.FlowLayoutPanel topPanel;
         private System.Windows.Forms.Panel close;
         private System.Windows.Forms.Panel min;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label title;
         private System.Windows.Forms.TrackBar gammaBar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button setButton;
         private System.Windows.Forms.TextBox gammaTextBox;
+        private System.Windows.Forms.Panel info;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- MinecraftGammaSetting/MinecraftGammaSetting/main.cs
+++ MinecraftGammaSetting/MinecraftGammaSetting/main.cs
@@ -1,69 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿// Program Ver: 1
+// Last Update: 01/02/2020
+// Developer: Nitro
+// E-Mail: nitro0@naver.com
+// License: MIT License
+// Open-Source: https://github.com/Nitro1231/Minecraft-Gamma-Editor
+
+using System;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MinecraftGammaSetting {
     public partial class main : Form {
         private string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\options.txt";
         private string[] option;
-        private int gamma;
-       
+        private int gamma, gammaIndex;
+        private bool infoButton = false;
+
         private char colon = ':';
         public main() {
             if (!File.Exists(path)) {
-                MessageBox.Show("Cannot find .minecraft\\options.txt file.\nThis file is necessary to change the gamma value.\nPlease install the Minecraft, and if you already did, change the Minecraft version or re-install the game.", "File dose not exist.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error: File dose not exist.\nCannot find .minecraft\\options.txt file.\nThis file is necessary to change the gamma value.\nPlease install the Minecraft, and if you already did, change the Minecraft version or re-install the game.", "Minecraft Gamma Editor", MessageBoxButtons.OK, MessageBoxIcon.Error);
             } else {
                 option = File.ReadAllLines(path);
+                int i = 0;
                 foreach (string l in option) {
-                   if (l.Contains("gamma")) {
+                    if (l.Contains("gamma")) {
                         gamma = (int)float.Parse(l.Split(colon)[1]);
+                        gammaIndex = i;
                         break;
                     }
+                    i++;
                 }
             }
             InitializeComponent();
-            Utils.smoothBorder(close, close.Width);
-            Utils.smoothBorder(min, min.Width);
+            Height = 155;
             Utils.smoothBorder(this, 10);
+            Utils.smoothBorder(close, close.Height);
+            Utils.smoothBorder(min, min.Height);
+            Utils.smoothBorder(info, info.Height);
             gammaTextBox.Text = gamma.ToString();
             gammaBar.Value = gamma;
         }
 
-        private void Set_Click(object sender, EventArgs e) {
-            //label3.Text = path;
+        private void setButton_Click(object sender, EventArgs e) {
+            if (!File.Exists(path)) {
+                MessageBox.Show("Error: File dose not exist.\nCannot find .minecraft\\options.txt file.\nThis file is necessary to change the gamma value.\nPlease install the Minecraft, and if you already did, change the Minecraft version or re-install the game.", "Minecraft Gamma Editor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else {
+                option[gammaIndex] = $"gamma:{gamma}";
+                try {
+                    File.WriteAllLines(path, option);
+                    MessageBox.Show($"Gamma value has been changed successfully!\nIf you are playing Minecraft now, please restart the game.", "Minecraft Gamma Editor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } catch (Exception ex) {
+                    MessageBox.Show($"An error has occurred while changing the gamma value.\nDetail information: {ex.ToString()}", "Minecraft Gamma Editor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void gammaBar_Scroll(object sender, EventArgs e) {
-            gammaTextBox.Text = gammaBar.Value.ToString();
+            gamma = int.Parse(gammaBar.Value.ToString());
+            gammaTextBox.Text = gamma.ToString();
         }
 
         private void gammaTextBox_TextChanged(object sender, EventArgs e) {
             if (gammaTextBox.Text == "")
                 gammaTextBox.Text = "1";
-            int v = int.Parse(gammaTextBox.Text);
-            if (v < 1) {
+            gamma = int.Parse(gammaTextBox.Text);
+            if (gamma < 1) {
+                gamma = 1;
                 gammaTextBox.Text = "1";
                 gammaBar.Value = 1;
-            } else if (v > 1000) {
+            } else if (gamma > 1000) {
+                gamma = 1000;
                 gammaTextBox.Text = "1000";
                 gammaBar.Value = 1000;
             }
-            gammaBar.Value = int.Parse(gammaTextBox.Text);
+            gammaBar.Value = gamma;
+        }
+
+        private void Info_Click(object sender, EventArgs e) {
+            if (!infoButton) {
+                while (Height <= 296) {
+                    Height += 4;
+                    Refresh();
+                }
+                Height = 300;
+                infoButton = true;
+            } else {
+                while (Height >= 159) {
+                    Height -= 4;
+                    Refresh();
+                }
+                Height = 155;
+                infoButton = false;
+            }
         }
 
         private void Close_Click(object sender, EventArgs e) { Close(); }
         private void Min_Click(object sender, EventArgs e) { WindowState = FormWindowState.Minimized; }
         private void Main_MouseMove(object sender, MouseEventArgs e) { Utils.mouseMove(Handle); }
+        private void PictureBox1_Click(object sender, EventArgs e) { Process.Start("https://github.com/Nitro1231/Minecraft-Gamma-Editor"); }
+        private void PictureBox2_Click(object sender, EventArgs e) { Process.Start("https://nitro1231.github.io/"); }
+        private void PictureBox3_Click(object sender, EventArgs e) { Process.Start("https://nitr0.tistory.com/"); }
+        private void Label3_Click(object sender, EventArgs e) { Process.Start("https://icons8.com/"); }
         private void gammaTextBox_KeyPress(object sender, KeyPressEventArgs e) {
             if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
                 e.Handled = true;
+        }
+        private void Main_Resize(object sender, EventArgs e) {
+            Utils.smoothBorder(this, 10);
+            Refresh();
         }
     }
 }
